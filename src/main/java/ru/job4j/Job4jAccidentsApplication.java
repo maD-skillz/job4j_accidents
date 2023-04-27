48ca851a29fba6571525305b317d8ecafe7f7da0
maD-skillz <warneverchang3s@gmail.com>
2023-04-27 21:36:53 +0300

    0. Spring MVC [#6877 #412495]

--- src/main/java/ru/job4j/Job4jAccidentsApplication.java
+++ src/main/java/ru/job4j/Job4jAccidentsApplication.java
@@ -1,11 +1,11 @@
 package ru.job4j;
 
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import ru.job4j.controller.IndexController;
+import org.springframework.boot.SpringApplication;
 
 public class Job4jAccidentsApplication {
 
     public static void main(String[] args) {
+        SpringApplication.run(Job4jAccidentsApplication.class, args);
     }
 }
 
