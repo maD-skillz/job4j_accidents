3b53e317086b7f6fd9c01b3f7386fae44b5948c8
maD-skillz <warneverchang3s@gmail.com>
2023-04-28 12:11:28 +0300

    0. Spring MVC [#6877 #412495] - pom.xml fix

--- src/main/java/ru/job4j/Job4jAccidentsApplication.java
+++ src/main/java/ru/job4j/Job4jAccidentsApplication.java
@@ -1,11 +1,14 @@
 package ru.job4j;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class Job4jAccidentsApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(Job4jAccidentsApplication.class, args);
+        System.out.println("Go to http://localhost:8080/index");
     }
 }
 
